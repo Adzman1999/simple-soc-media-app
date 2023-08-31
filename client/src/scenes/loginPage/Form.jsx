1bc39361e5d1376b94c127381a0b0fcebbe47a9a
Mhan1999 <abdulsamadadzmani@gmail.com>
2023-08-31 18:52:36 +0800

    added axios lib

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -112,28 +112,6 @@ const Form = () => {
     }
   };
 
-  // const login = async (values, onSubmitProps) => {
-  //   const loggedInResponse = await fetch(
-  //     "http://simple-soc-media-pf.onrender.com:8200/auth/login",
-  //     {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify(values),
-  //     }
-  //   );
-  //   const loggedIn = await loggedInResponse.json();
-  //   onSubmitProps.resetForm();
-  //   if (loggedIn) {
-  //     dispatch(
-  //       setLogin({
-  //         user: loggedIn.user,
-  //         token: loggedIn.token,
-  //       })
-  //     );
-  //     navigate("/home");
-  //   }
-  // };
-
   const handleFormSubmit = async (values, onSubmitProps) => {
     setLoading(true);
     try {
